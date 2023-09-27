52e73d2dd3d46775ab32d5b6f69811487e2d30ba
Inhar Serrano <inhar.serrano@opendeusto.es>
2023-09-27 10:38:45 +0200

    Añadido texto "Prueba Exitosa"

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!");
 		System.out.println("Prueba de Funcionamiento");
+		System.err.println("Prueba Exitosa");
 	}
 }
